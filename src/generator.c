e82c3153ccf0dad741c6d4ace5ac688de112c450
AlexisEnglebert <englebert.alexis@gmail.com>
2026-03-12 20:41:34 +0100

    speed up process open

--- src/generator.c
+++ src/generator.c
@@ -4,6 +4,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+
 
 #include "generator.h"
 #include "tar.h"
@@ -13,53 +15,70 @@ int run_test(tar_t* a, const char* command_prefix) {
     //runs the test and compute the checksum for the tar header 
     calculate_checksum(a);
     write_tar(a);
+    (void) command_prefix;
     execute_extractor(command_prefix);
     
     return 0;
 }
 
 int execute_extractor(const char* command_prefix) {
-    char executable[100];
+    char executable[256];
     
-    strcpy(executable, command_prefix);
-    strcat(executable, command);
+    snprintf(executable, sizeof(executable), "%s%s", command_prefix, command);
 
-    strcat(executable, " archive.tar");
-    FILE *fp = fp = popen(executable, "r");
-    
-    if (fp == NULL) {
-        printf("Error opening pipe: %s\n", strerror(errno));
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        fprintf(stderr, "Error creating pipe\n");
         return -1;
     }
-    
-    char program_output[33];
-    int return_value = 0;
-    if(fgets(program_output, 33, fp) == NULL) {
-        goto close_pipe;
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        fprintf(stderr, "Error during fork\n");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
     }
 
-    if(strncmp(program_output, "*** The program has crashed ***\n", 33)) {
-        printf("---------- FOUND A BUG --------------\n");
-        goto close_pipe;
+    if (pid == 0) {
+        close(pipefd[0]);
+        if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+            fprintf(stderr, "Error during dup2\n");
+            return -1;
+        }
+        close(pipefd[1]);
+        char *args[] = {
+            executable,
+            "archive.tar",
+            NULL
+        };
+        execvp(executable, args);        
+        fprintf(stderr, "Error executing program : %s\n", strerror(errno));
+        return -1;
     } else {
-        return_value = -1;
-        goto close_pipe;
-    }
+        close(pipefd[1]);
+        FILE *fp = fdopen(pipefd[0], "r");
+        char program_output[64] = {0};
+        int rv = -1;
 
-close_pipe:
-    int ret = pclose(fp);
-    if(ret == -1) {
-        fprintf(stderr, "Error closing pipe: %s\n", strerror(errno));
-        return_value = -1;
-    }
+        if (fgets(program_output, sizeof(program_output), fp) != NULL) {
+                if (strncmp(program_output, "*** The program has crashed ***", 31) == 0) {
+                    printf("---------- FOUND A BUG --------------\n");
+                    rv = 0;
+                }
+            }
+        fclose(fp);
 
-    return return_value;
+        int status;
+        waitpid(pid, &status, 0);
+
+        return rv;
+    }
+    return -1;
 }
 
 
-
 int fuzz_name(tar_t* tar) {
-
     mkdir("trash", 0777);
 
     if (chdir("trash") != 0) {
@@ -128,16 +147,17 @@ int fuzz_8bits(tar_t* a, char* field) {
     }
     return 0;
 }
+
 int generate_inputs() {
     
     tar_t candidate = {0};
     init_valid_tar(&candidate);
+    fuzz_name(&candidate);
 
     fuzz_8bits(&candidate, candidate.mode);
     fuzz_8bits(&candidate, candidate.uid);
     fuzz_8bits(&candidate, candidate.gid);
 
-    fuzz_name(&candidate);
     
 
 
