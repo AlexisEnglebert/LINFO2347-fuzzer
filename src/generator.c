4b62be8b002700dd64bc525f94db407b44f7218b
victor <vfairon@gmail.com>
2026-03-12 18:07:41 +0100

    8 bit fuzzing

--- src/generator.c
+++ src/generator.c
@@ -5,6 +5,16 @@
 #include "generator.h"
 #include "tar.h"
 
+
+int run_test(tar_t* a, const char* path) {
+    //runs the test and compute the checksum for the tar header 
+    calculate_checksum(a);
+    write_tar(a);
+    execute_extractor(path);
+    
+    return 0;
+}
+
 int execute_extractor(const char* path) {
     char command[51];
     strcpy(command, path);
@@ -46,15 +56,55 @@ int permute_name(tar_t* a) {
     
     return 0;
 }
+int fuzz_8bits(tar_t* a, const char* path, char* field) {
+    //testing negative number : 
+    strcpy(field, "-1234567");
+    run_test(a, path);
 
+    //testing not number 
+    strcpy(field, "abcdefg");
+    run_test(a, path);
+
+    //embeded null : 
+    memcpy(field, "A\0AAAAA", 8);
+    run_test(a, path);
+
+    //not null terminated :
+    memset(field, 'A', 8);
+    run_test(a, path);
+    
+    memcpy(field, "   644  ", 8);
+    run_test(a, path);
+
+    //All spaces
+    memset(field, ' ', 8);
+    run_test(a, path);
+
+    memcpy(field, "8888888", 8);
+    run_test(a, path);
+
+    // for (int pos = 0; pos < 8; pos++) {
+    //     for (int c = 0; c < 256; c++) {
+    //         memcpy(field, "0000755", 8); // baseline
+    //         field[pos] = c;
+
+    //         run_test(a, path);
+    //     }
+    // }
+    return 0;
+}
 int generate_inputs(const char* path) {
     (void)path;
     
     tar_t candidate = {0};
 
     init_valid_tar(&candidate);
-    // TODO ....
-    write_tar(&candidate);
+
+    fuzz_8bits(&candidate, path, candidate.mode);
+    fuzz_8bits(&candidate, path, candidate.uid);
+    fuzz_8bits(&candidate, path, candidate.gid);
+
+
 
     return 0;
 }
--- src/generator.h
+++ src/generator.h
@@ -1,5 +1,8 @@
+#include "tar.h"
 #pragma once
 
 int execute_extractor(const char* path);
 
 int generate_inputs(const char* path);
+
+int fuzz_mode(tar_t* a, const char* path);
